f1c0977e3085c4173506154ae7c89a09c2af3bbe
ankitsingh49578 <ankitsingh49578@gmail.com>
2025-03-25 11:35:18 +0530

    node modules update

--- News-App/src/components/Navbar.jsx
+++ News-App/src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-function Navbar({setCategory}) {
+function Navbar({ setCategory }) {
   return (
     <div className='flex flex-row gap-10 bg-[#616161] py-4 px-4 select-none'>
-        <div className='cursor-pointer'>
-            <div className='bg-[#e0dfdf] p-1 rounded-md px-2 font-bold text-xl'>
-                NewsTime
-            </div>
+      <div className='cursor-pointer'>
+        <div className='bg-[#e0dfdf] p-1 rounded-md px-2 font-bold text-xl'>
+          NewsTime
         </div>
-        <ul className='flex flex-row justify-evenly gap-5 text-[#e0dfdf] text-[18px] font-semibold'>
-            <li onClick={()=>setCategory("technology")} className='cursor-pointer hover:text-yellow-100 group'> Technology <div className='h-[2px] w-full group-hover:bg-white'></div></li>
-            <li onClick={()=>setCategory("business")} className='cursor-pointer hover:text-yellow-100 group:'> Business <div className='h-[2px] w-full group-hover:bg-white'></div></li>
-            <li onClick={()=>setCategory("health")} className='cursor-pointer hover:text-yellow-100 group'> Health <div className='h-[2px] w-full group-hover:bg-white'></div></li>
-            <li onClick={()=>setCategory("science")} className='cursor-pointer hover:text-yellow-100 group'> Science <div className='h-[2px] w-full group-hover:bg-white'></div></li>
-            <li onClick={()=>setCategory("entertainment")} className='cursor-pointer hover:text-yellow-100 group'> Entertainment <div className='h-[2px] w-full group-hover:bg-white'></div></li>
-        </ul>
+      </div>
+      <ul className='flex flex-row justify-evenly gap-5 text-[#e0dfdf] text-[18px] font-semibold'>
+        <li onClick={() => setCategory("technology")} className='cursor-pointer hover:text-yellow-100 group'> Technology <div className='h-[2px] w-full group-hover:bg-white'></div></li>
+        <li onClick={() => setCategory("business")} className='cursor-pointer hover:text-yellow-100 group:'> Business <div className='h-[2px] w-full group-hover:bg-white'></div></li>
+        <li onClick={() => setCategory("health")} className='cursor-pointer hover:text-yellow-100 group'> Health <div className='h-[2px] w-full group-hover:bg-white'></div></li>
+        <li onClick={() => setCategory("science")} className='cursor-pointer hover:text-yellow-100 group'> Science <div className='h-[2px] w-full group-hover:bg-white'></div></li>
+        <li onClick={() => setCategory("entertainment")} className='cursor-pointer hover:text-yellow-100 group'> Entertainment <div className='h-[2px] w-full group-hover:bg-white'></div></li>
+      </ul>
     </div>
   )
 }
